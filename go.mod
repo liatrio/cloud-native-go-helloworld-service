f7e84347a3c70447e950b11649b9977e8abed38c
Alex Ashley <alexa@liatrio.com>
2020-10-19 16:55:51 -0500

    Instrument with Prometheus and add a custom counter

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/liatrio/cloud-native-go-helloworld-service
+
+go 1.15
+
+require (
+	github.com/prometheus/client_golang v1.8.0
+	github.com/sirupsen/logrus v1.6.0
+)
--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 	"os"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello Liatrio!</h1>"))
+	_, err := w.Write([]byte("<h1>Hello Liatrio!</h1>"))
+
+	log.Debugf("Request received on %s", r.RequestURI)
+
+	if err != nil {
+		log.Errorf("error sending response: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	hellosGiven.Inc()
 }
+
+var (
+	hellosGiven = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "cloud_native_go_helloworld_service_hellos_given_total",
+		Help: "The total number of hellos returned by the service",
+	})
+)
 
 func main() {
 	port := os.Getenv("PORT")
@@ -15,8 +36,12 @@ func main() {
 		port = "3000"
 	}
 
-	mux := http.NewServeMux()
+	log.SetLevel(log.DebugLevel)
 
+	mux := http.NewServeMux()
 	mux.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":"+port, mux)
+	mux.Handle("/metrics", promhttp.Handler())
+
+	log.Infof("Server starting on port: %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
